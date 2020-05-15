84816ac5cba76c58ccf98dad3225ab871fbf95fd
nmdas <60233841+nmdas@users.noreply.github.com>
2020-05-15 09:27:04 -0500

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,136 @@
+export default {
+    products:[
+        {
+        _id: '1',    
+        name: 'EARBUDS 1',
+        category: 'EARBUDS',
+        image: '/images/p2.png',
+        price: 80,
+        brand: 'RUNE PODS',
+        rating: 4.7,
+        numReviews: 15,
+        countInStock: 5
+        },
+        {
+            _id: '2',
+        name: 'EARBUDS 2',
+        category: 'EARBUDS',
+        image: '/images/p3.png',
+        price: 70,
+        brand: 'RUNE PODS',
+        rating: 4.5,
+        numReviews: 12,
+        countInStock: 5
+        },
+        {
+            _id: '3',
+        name: 'EARBUDS 3',
+        category: 'EARBUDS',
+        image: '/images/p4.png',
+        price: 59,
+        brand: 'RUNE PODS',
+        rating: 4.3,
+        numReviews: 10,
+        countInStock: 5
+        },
+        {
+            _id: '4',
+        name: 'EARBUDS 4',
+        category: 'EARBUDS',
+        image: '/images/p5.png',
+        price: 69,
+        brand: 'RUNE PODS',
+        rating: 3.7,
+        numReviews: 7,
+        countInStock: 5
+        },
+        {
+            _id: '5',
+        name: 'EARBUDS 5',
+        category: 'EARBUDS',
+        image: '/images/p8.png',
+        price: 75,
+        brand: 'RUNE PODS',
+        rating: 3.5,
+        numReviews: 11,
+        countInStock: 5
+        },
+        {
+            _id: '6',
+        name: 'EARBUDS 6',
+        category: 'EARBUDS',
+        image: '/images/p7.png',
+        price: 65,
+        brand: 'RUNE PODS',
+        rating: 3.3,
+        numReviews: 8,
+        countInStock: 5
+        },
+        {
+        _id: '1',    
+        name: 'EARBUDS 1',
+        category: 'EARBUDS',
+        image: '/images/p2.png',
+        price: 80,
+        brand: 'RUNE PODS',
+        rating: 4.7,
+        numReviews: 15,
+        countInStock: 5
+        },
+        {
+            _id: '2',
+        name: 'EARBUDS 2',
+        category: 'EARBUDS',
+        image: '/images/p3.png',
+        price: 70,
+        brand: 'RUNE PODS',
+        rating: 4.5,
+        numReviews: 12,
+        countInStock: 5
+        },
+        {
+            _id: '3',
+        name: 'EARBUDS 3',
+        category: 'EARBUDS',
+        image: '/images/p4.png',
+        price: 59,
+        brand: 'RUNE PODS',
+        rating: 4.3,
+        numReviews: 10,
+        countInStock: 5
+        },
+        {
+            _id: '4',
+        name: 'EARBUDS 4',
+        category: 'EARBUDS',
+        image: '/images/p5.png',
+        price: 69,
+        brand: 'RUNE PODS',
+        rating: 3.7,
+        numReviews: 7,
+        countInStock: 5
+        },
+        {
+            _id: '5',
+        name: 'EARBUDS 5',
+        category: 'EARBUDS',
+        image: '/images/p8.png',
+        price: 75,
+        brand: 'RUNE PODS',
+        rating: 3.5,
+        numReviews: 11,
+        countInStock: 5
+        },
+        {
+            _id: '6',
+        name: 'EARBUDS 6',
+        category: 'EARBUDS',
+        image: '/images/p7.png',
+        price: 65,
+        brand: 'RUNE PODS',
+        rating: 3.3,
+        numReviews: 8,
+        countInStock: 5
+        }
+    ]
+}
